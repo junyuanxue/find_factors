0ae05632710b31633eb3a5dc23813f4d3ef8e0ff
Junyuan Xue <junyuanxue.is@gmail.com>
2016-02-23 18:53:48 +0000

    Edited find_factors. All tests passed.

--- lib/find_factors.rb
+++ lib/find_factors.rb
@@ -1,5 +1,10 @@
 # Codewars kata: http://www.codewars.com/kata/564fa92d1639fbefae00009d/train/ruby
 
 def factors(x)
-  -1
+  return -1 if (!x.is_a? Integer) || (x < 1)
+  output = []
+  for i in 1..x
+    output << i if x % i == 0
+  end
+  output.reverse
 end
